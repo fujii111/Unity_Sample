7f5d861284d8ff33051545a08ce1fdac1d870f32
fujii111 <koutarou.fujii@gmail.com>
2019-06-25 00:54:12 +0900

    [add] chapter 11

---
+++ Rockfall/Assets/ShipWeapons.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipWeapons : MonoBehaviour {
+
+	public GameObject shotPrefab;
+
+	public void Awake() {
+		InputManager.instance.SetWeapons(this);
+	}
+
+	public void OnDestroy() {
+		if (Application.isPlaying == true) {
+			InputManager.instance.RemoveWeapons(this);
+		}
+	}
+
+	public Transform[] firePoints;
+	private int firePointIndex;
+
+	public void Fire() {
+		if (firePoints.Length == 0) {
+			return;
+		}
+
+		var firePointToUse = firePoints[firePointIndex];
+		Instantiate(shotPrefab, 
+			firePointToUse.position, 
+			firePointToUse.rotation);
+
+//		var audio = firePointToUse.GetComponent<AudioSource>();
+//		if (audio) {
+//			audio.Play();
+//		}
+
+		firePointIndex++;
+
+		if (firePointIndex >= firePoints.Length) {
+			firePointIndex = 0;
+		}
+	}
+}
