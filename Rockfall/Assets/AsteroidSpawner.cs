9751901e3fddceb8a6227f622b914e279a3d7929
fujii111 <koutarou.fujii@gmail.com>
2019-06-30 23:18:40 +0900

    [add] chapter12

---
+++ Rockfall/Assets/AsteroidSpawner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour {
+
+	public float radius = 250.0f;
+	public Rigidbody asteroidPrefab;
+	public float spawnRate = 5.0f;
+	public float variance = 1.0f;
+	public Transform target;
+	public bool spawnAsteroids = false;
+
+	void Start () {
+		StartCoroutine(CreateAsteroids());
+	}
+
+	IEnumerator CreateAsteroids() {
+		while (true) {
+			float nextSpawnTime = spawnRate + Random.Range(-variance, variance);
+			yield return new WaitForSeconds(nextSpawnTime);
+			yield return new WaitForFixedUpdate();
+			CreateNewAsteroid();
+		}
+	}
+
+	void CreateNewAsteroid() {
+		if (spawnAsteroids == false) {
+			return;
+		}
+
+		var asteroidPosition = Random.onUnitSphere * radius;
+		asteroidPosition.Scale(transform.lossyScale);
+		asteroidPosition += transform.position;
+		var newAsteroid = Instantiate(asteroidPrefab);
+		newAsteroid.transform.position = asteroidPosition;
+		newAsteroid.transform.LookAt(target);
+	}
+
+	void OnDrawGizmosSelected() {
+		Gizmos.color = Color.yellow;
+		Gizmos.matrix = transform.localToWorldMatrix;
+		Gizmos.DrawWireSphere(Vector3.zero, radius);
+	}
+		
+	public void DestroyAllAsteroids() {
+		foreach (var asteroid in FindObjectsOfType<Asteroid>()) {
+			Destroy (asteroid.gameObject);
+		}
+	}	
+}
---
+++ Rockfall/Assets/DamageOnCollide.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnCollide : MonoBehaviour {
+
+	public int damage = 1;
+	public int damageToSelf = 5;
+
+	void HitObject(GameObject theObject) {
+
+		var theirDamage = theObject.GetComponentInParent<DamageTaking>();
+		if (theirDamage) {
+			theirDamage.TakeDamage(damage);
+		}
+		
+		var ourDamage = this.GetComponentInParent<DamageTaking>();
+		if (ourDamage) {
+			ourDamage.TakeDamage(damageToSelf);
+		}
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		HitObject(collider.gameObject);
+	}
+
+	void OnCollisionEnter(Collision collision) {		
+		HitObject(collision.gameObject);
+	}
+}
